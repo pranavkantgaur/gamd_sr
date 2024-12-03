fix r-6, r-12 prior implementation with min-max scaling

reference_gamd.py
@@ -597,8 +597,13 @@ class SimpleMDNetNew(nn.Module):  # no bond, no learnable node encoder
         self.box_size = self.box_size
 
         self.node_emb = nn.Parameter(torch.randn((1, encoding_size)), requires_grad=True)
-        self.edge_encoder = MLP(9 + 1 + len(self.edge_expand.centers), self.edge_emb_dim, hidden_dim=hidden_dim,
+        '''
+        self.edge_encoder = MLP(3 + 1 + len(self.edge_expand.centers), self.edge_emb_dim, hidden_dim=hidden_dim,
+                                activation='gelu')        
+        '''
+        self.edge_encoder = MLP(3 + 2 + 1 + len(self.edge_expand.centers), self.edge_emb_dim, hidden_dim=hidden_dim,
                                 activation='gelu')
+        
         self.edge_layer_norm = nn.LayerNorm(self.edge_emb_dim)
         self.graph_decoder = MLP(encoding_size, out_feats, hidden_layer=2, hidden_dim=hidden_dim, activation='gelu')
 
@@ -630,19 +635,34 @@ class SimpleMDNetNew(nn.Module):  # no bond, no learnable node encoder
             self._update_length_stat(self.length_scaler.mean_, np.sqrt(self.length_scaler.var_))
 
         rel_pos_norm = (rel_pos_norm - self.length_mean) / self.length_std
-
-        # Add inductive bias like r^-6, r^-12 to signal intramolecular potentials       
-        r_neg_6_prior = rel_pos.pow(-6)
+        
+        
+        # Add inductive bias like r^-6, r^-12 to signal intramolecular potentials      
+        
+        r_neg_6_prior = rel_pos_norm.pow(-6)
         # Replace inf values with 0
         r_neg_6_prior[torch.isinf(r_neg_6_prior)] = 0.0
-        r_neg_12_prior = rel_pos.pow(-12)
+        r_neg_12_prior = rel_pos_norm.pow(-12)
         r_neg_12_prior[torch.isinf(r_neg_12_prior)] = 0.0
 
+        
         edge_feat = torch.cat((rel_pos_periodic,
                                r_neg_6_prior,
                                r_neg_12_prior,
                                rel_pos_norm,                               
                                self.edge_expand(rel_pos_norm)), dim=1)
+        
+        
+        # Min-Max Scaling to avoid Nans downstream
+        min_val = torch.min(edge_feat)
+        max_val = torch.max(edge_feat)
+        edge_feat = (edge_feat - min_val) / (max_val - min_val)
+        
+        '''
+        edge_feat = torch.cat((rel_pos_periodic,
+                               rel_pos_norm,                               
+                               self.edge_expand(rel_pos_norm)), dim=1) 
+        '''
         return edge_feat
 
     def build_graph(self,
@@ -655,7 +675,12 @@ class SimpleMDNetNew(nn.Module):  # no bond, no learnable node encoder
         fluid_graph = dgl.graph((neigh_idx, center_idx))
         fluid_edge_feat = self.calc_edge_feat(center_idx, neigh_idx, fluid_pos)
 
+        check = self.edge_encoder(fluid_edge_feat)
+
+        
+
         fluid_edge_emb = self.edge_layer_norm(self.edge_encoder(fluid_edge_feat))  # [edge_num, 64]
+        
         fluid_edge_emb = self.edge_drop_out(fluid_edge_emb)
         fluid_graph.edata['e'] = fluid_edge_emb
 